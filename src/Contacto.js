aafc03536db3d6f75c580029b83f4f46a291ade6
bryanjsilva <bryanzsilva@hotmail.com>
2021-05-10 23:05:00 -0500

    Corrección de seguridad en enlaces de contacto

--- src/Contacto.js
+++ src/Contacto.js
@@ -12,19 +12,19 @@ export default class Contacto extends Component {
                     <p>Hemos creado una aplicación en donde encontrarás diferentes lugares y las rutas que deberás seguir para llegar. Además cuenta con un sistema de rating y comentarios que te permitirán conocer las opiniones de otros usuarios.</p><br/>
                     <h2>¡Síguenos!</h2><br/>
                     <section className='d-flex justify-content-around'>
-                        <a href='https://www.facebook.com' target='_blank'>
+                        <a href='https://www.facebook.com' target='_blank' rel="noreferrer">
                             <i className='fa fa-facebook fa-fw'></i>
                         </a>
-                        <a href='https://www.linkedin.com' target='_blank'>
+                        <a href='https://www.linkedin.com' target='_blank' rel="noreferrer">
                             <i className='fa fa-linkedin fa-fw'></i>
                         </a>
-                        <a href='https://www.instagram.com' target='_blank'>
+                        <a href='https://www.instagram.com' target='_blank' rel="noreferrer">
                             <i className='fa fa-instagram fa-fw'></i>
                         </a>
-                        <a href='https://www.twitter.com' target='_blank'>
+                        <a href='https://www.twitter.com' target='_blank' rel="noreferrer">
                             <i className='fa fa-twitter fa-fw'></i>
                         </a>
-                        <a href='https://www.github.com/bryanjsilva' target='_blank'>
+                        <a href='https://www.github.com/bryanjsilva' target='_blank' rel="noreferrer">
                             <i className='fa fa-github fa-fw'></i>
                         </a>
                     </section>
