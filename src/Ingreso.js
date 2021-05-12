03184bc4f72994bec65c59a5ecc48e147ab47a7c
bryanjsilva <bryanzsilva@hotmail.com>
2021-05-12 11:04:26 -0500

    Solucionado google login, e integran las funciones al archivo Ingreso

--- src/Ingreso.js
+++ src/Ingreso.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import logo from './img/logo-min.png'
 import './Ingreso.css'
-import GoogleLogin from './GoogleLogin'
 
 export default class Contacto extends Component {
 
@@ -33,6 +32,8 @@ export default class Contacto extends Component {
             logged: true,
         })
         this.props.statusIngreso(true)
+        console.log(this.state.logged)
+        console.log(this.props.statusIngreso)
     }
 
     manejarOnClick = (evento) => {
@@ -62,6 +63,12 @@ export default class Contacto extends Component {
         if(evento.target.id==='salir'){
             this.setState({logged: false})
             this.props.statusIngreso(false)
+            this.logout()
+        }
+        if(evento.target.id==='google'){
+            if(this.state.logged===false){
+                this.login()
+            }
         }
     }
     
@@ -81,6 +88,54 @@ export default class Contacto extends Component {
         }
     }
 
+    login = () => {
+        const clientParam = {client_id: '874945298095-t4llq3ak7hs1rk5ivs08bd1iiifoilcc.apps.googleusercontent.com'}
+        this.gapi.auth2.init(clientParam)
+        const auth = this.gapi.auth2.getAuthInstance();
+        const loggedInGoogle = auth.isSignedIn.get();
+        if(!loggedInGoogle){
+            auth.signIn().then(
+                (success) => {
+                    this.setState({logged:true})
+                    this.props.statusIngreso(true)
+                    console.log(this.state.logged)
+                    console.log(this.props.statusIngreso)
+                },
+                (error) => {
+                    this.setState({logged:false})
+                    this.props.statusIngreso(false)
+                }
+            )
+        }else{
+            this.setState({logged:true})
+            this.props.statusIngreso(false)
+        }
+    }
+
+    logout = () => {
+        const auth2 = this.gapi.auth2.getAuthInstance();
+        auth2.signOut().then(
+            (success) => {
+                this.setState({logged:false})
+                this.props.statusIngreso(false)
+            },
+            (error) => {
+                console.log('Error al salir con google')
+            }
+        )
+        auth2.disconnect();
+    }
+
+    componentDidMount = () => {
+        const googleSignAPILoad = setInterval(() => {
+            if(window.gapi){
+                this.gapi = window.gapi
+                clearInterval(googleSignAPILoad)
+                this.gapi.load('auth2',function(){})
+            };
+        },100)
+    }
+
     render(){
         if(this.state.logged){
             return(
@@ -89,7 +144,7 @@ export default class Contacto extends Component {
                         href='/#'
                         id='salir' 
                         className='btn btn-outline-dark'
-                        onClick={this.manejarOnClick}>Salir</a>
+                        onClick={this.manejarOnClick}>Salir <i className='fa fa-sign-out fa-fw'></i></a>
                 </div>
             )
         }else{
@@ -132,11 +187,14 @@ export default class Contacto extends Component {
                                     onClick={this.manejarOnClick}>
                                         Ingresar &nbsp;<span><i className='fa fa-sign-in fa-fw'></i></span>
                                 </a>
+                                <a 
+                                    className='btn btn-dark color-light w-100 mt-2'
+                                    href = '/#'
+                                    id='google'
+                                    onClick={this.manejarOnClick}>
+                                        Ingresar con Google <span><i className='fa fa-google fa-fw'></i></span>
+                                </a>
                             </form>
-                            <hr/>
-                            <GoogleLogin 
-                                logged={this.props.logged}
-                                statusIngreso={this.props.statusIngreso}/>
                         </div>
                     </div>
                 </div>
